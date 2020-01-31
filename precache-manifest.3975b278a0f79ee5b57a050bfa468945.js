811426f29f526e5d65fd5dac6f9ac0803402be0b
RDXKIRITO <trialasp9@gmail.com>
2020-01-31 17:57:28 +0530

    Updates

---
+++ precache-manifest.3975b278a0f79ee5b57a050bfa468945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188c18a048014c28ad2d4747851f8d4a",
+    "url": "/aire/index.html"
+  },
+  {
+    "revision": "e73651b2c86d8769f952",
+    "url": "/aire/static/css/main.726d11b3.chunk.css"
+  },
+  {
+    "revision": "67eb8c33c8706590ef95",
+    "url": "/aire/static/js/2.b4c8ef80.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/aire/static/js/2.b4c8ef80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e73651b2c86d8769f952",
+    "url": "/aire/static/js/main.29e402a2.chunk.js"
+  },
+  {
+    "revision": "79c003802b44a459d273",
+    "url": "/aire/static/js/runtime-main.67629300.js"
+  }
+]);
